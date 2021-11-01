7f9bc759bdb50b2129b85f522e748e2d46ba3ac6
Stanislav-Vorozhun <stanislav.vorozhun@gmail.com>
2021-11-01 21:55:23 +0300

    less 124 final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Alert } from "./components/Alert";
 import { Navbar } from "./components/Navbar";
+import { AlertState } from "./context/alert/alertState";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
@@ -8,16 +10,20 @@ import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div className='container pt-4'>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile/:name" component={Profile} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className='container pt-5'>
+          <Alert alert={{ text: "test alert" }} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/profile/:name" component={Profile} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+
+    </AlertState>
 
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Card } from "../components/Card";
+import { Search } from "../components/Search";
 
 export const Home = () => {
+    const cardsArray = new Array(15).fill("").map((_, i) => i)
     return (
-        <div>
-            <h1>Home page</h1>
-      
-    </div>
+        <Fragment>
+            <Search />
+            <div className="row">
+                {cardsArray.map(card => {
+                    return (
+                        <div className="col-sm-3 mb-4" key={card}>
+                            <Card />
+                        </div>)
+                })}
+            </div>
+        </Fragment>
     )
 }

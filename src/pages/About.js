19c6cf6685971c9d4659ae453c8f97a1586da964
Stanislav Vorozhun <stanislav.vorozhun@gmail.com>
2021-11-01 20:26:41 +0300

    first

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">Йоу! Тут изучают хуки</h1>
+                <p className="col-md-8 fs-4">Хуки — нововведение в React 16.8, которое позволяет использовать состояние и другие возможности React без написания классов.</p>
+                <button className="btn btn-primary btn-lg" type="button">Респект</button>
+            </div>
+        </div>
+    )
+}
